fdd11c4455a7e2d27793716258802ed7d60222f0
Tarek Alabd <tarekalabd1@gmail.com>
2024-11-20 20:47:13 +0200

    feat: quick look

--- lib/core/utils/app_constants.dart
+++ lib/core/utils/app_constants.dart
@@ -1,3 +1,9 @@
 class AppConstants {
-  static const appName = 'News App';
+  static const String appName = 'News App';
+
+  // APIs
+  static const String apiKey = '483cfdf815b34dd48e59c99e7767167f';
+  static const String baseUrl = 'https://newsapi.org';
+  static const String topHeadlines = '/v2/top-headlines';
+  static const String everything = '/v2/everything';
 }
