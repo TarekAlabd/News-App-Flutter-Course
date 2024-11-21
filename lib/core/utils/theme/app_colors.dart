f505ecc1b7ae5076b72b53f0cf676c25d70be38b
Tarek Alabd <tarekalabd1@gmail.com>
2024-11-21 07:43:34 +0200

    feat: enhancements on the carousel

--- lib/core/utils/theme/app_colors.dart
+++ lib/core/utils/theme/app_colors.dart
@@ -3,5 +3,6 @@ import 'package:flutter/material.dart';
 class AppColors {
   static const red = Colors.red;
   static const green = Colors.green;
+  static const black = Colors.black;
   static const primary = Colors.blue;
 }
